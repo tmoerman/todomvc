3249e23f7ecc0969f1c54f5ad39b7570521cb183
Luke VanderHart <luke@vanderhart.net>
2014-05-23 13:25:52 -0400

    update TodoMVC example

--- architecture-examples/quiescent/project.clj
+++ architecture-examples/quiescent/project.clj
@@ -1,10 +1,10 @@
 (defproject todomvc-quiescent "0.1.0"
-  :plugins [[lein-cljsbuild "1.0.1"]
+  :plugins [[lein-cljsbuild "1.0.3"]
             [lein-ring "0.8.10"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [quiescent/quiescent "0.1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2227"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
+                 [quiescent/quiescent "0.1.2-SNAPSHOT"]
                  [org.clojars.franks42/cljs-uuid-utils "0.1.3"]
                  [ring "1.2.1"]]
   :ring {:handler todomvc-quiescent.serve/handler}
