b13d80c7087d29301a692618f233f2d8830bc261
Thomas Moerman <thomas.moerman@gmail.com>
2016-03-28 22:21:12 +0200

    Kierros figwheel

---
+++ architecture-examples/kierros/project.clj
@@ -0,0 +1,40 @@
+(defproject kierros "0.1.0-SNAPSHOT"
+
+  :description "Kierros: Cycle-flavoured ClojureScript with Quiescent"
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.8.34"]
+                 [org.clojure/core.async "0.2.374" :exclusions [org.clojure/tools.reader]]
+                 [quiescent "0.3.1"]
+                 [org.clojars.franks42/cljs-uuid-utils "0.1.3"]]
+
+  :plugins [[lein-figwheel "0.5.2"]
+            [lein-cljsbuild "1.1.3" :exclusions [[org.clojure/clojure]]]]
+
+  ;; Note: it is not idiomatic to compile to, or serve from, the
+  ;; project root but TodoMVC wants the index.html there, so we'll
+  ;; adjust (dixit Quiescent)
+
+  :clean-targets ["resources/public/js/compiled" "target"]
+
+  :cljsbuild {:builds {:dev {:source-paths ["src"]
+                             :figwheel {:on-jsload "kierros.core/on-js-reload"}
+                             :compiler {:main kierros.core
+                                        :asset-path "js/compiled/out"
+                                        :output-dir "resources/public/js/compiled/out"
+                                        :output-to  "resources/public/js/compiled/kierros_main.js"
+                                        :optimizations :none
+                                        :source-map true}}
+
+                       :prod {:source-paths ["src"]
+                              :compiler {:output-to "main.js"
+                                         :source-map "main.js.map"
+                                         :optimizations :advanced
+                                         :pretty-print true}}}}
+
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]
+                                  [org.clojure/tools.nrepl "0.2.10"]
+                                  [figwheel-sidecar        "0.5.1"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}}
+
+  :figwheel {:nrepl-port 7888})
---
+++ architecture-examples/kierros/src/kierros/core.cljs
@@ -0,0 +1,19 @@
+(ns kierros.core
+  (:require [quiescent.core :as q]
+            [quiescent.dom :as d]))
+
+(enable-console-print!)
+
+(println "Edits to this text should show up in your developer console.")
+
+;; define your app data so that it doesn't get over-written on reload
+
+(defonce app-state (atom {:text "Hello world!"}))
+
+(println "Figgin' around like there's no tomorrow!")
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
