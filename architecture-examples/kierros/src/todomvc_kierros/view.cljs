7118c67dcb500d68a0490533b09cbb0eff38dc60
Thomas Moerman <thomas.moerman@gmail.com>
2016-04-01 21:45:59 +0200

    Anomaly solved! Cause: quiescent component argument list has positional semantics.

--- architecture-examples/kierros/src/todomvc_kierros/view.cljs
+++ architecture-examples/kierros/src/todomvc_kierros/view.cljs
@@ -1,13 +1,12 @@
 (ns todomvc-kierros.view
   (:require [cljs.core.async :as a :refer [>! chan pipe]]
-            [quiescent.core :as q]
+            [quiescent.core :as q :include-macros true]
             [quiescent.dom :as d])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (defn enter-key? [evt] (= 13 (.-keyCode evt)))
 
-(q/defcomponent
-  Header
+(q/defcomponent Header
   [state add-item-chan]
   (d/header {:id "header"}
             (d/h1 {} "todos")
@@ -21,50 +20,20 @@
                                          (-> evt .-target .-value (set! "")))))
                       :autoFocus true})))
 
-(q/defcomponent
-  State-display
-  [state]
-  (println (:items state))
-  (d/input {:id "state"
-            :placeholder (str state)
-            :on-change (fn [evt] (println (str "changed:" state)))
-               }))
-
-(q/defcomponent
-  Root
-  [{:keys [navigate
-           add-item
-           drop-item
-           clear-completed
-           toggle-completed
-           start-edit
-           end-edit] :as intent-chans}
-   state]
-  (do
-    (println "component called")
-    (d/div {:id      "main"
-            :checked true}
-           (Header state add-item)
-           (apply d/ul {:id      "todo-list"
-                        :checked true} (->> state
-                                            :items
-                                            (map #(d/li {} (d/label {} (:text %))))
-                                            )))))
-
-(defn vtree [intent-chans state]
-  (let [result (Root intent-chans state)]
-    (do
-      (println "vtree called")
-      result)))
+(q/defcomponent Root
+  [state {:keys [navigate add-item drop-item clear-completed toggle-completed start-edit end-edit]}]
+  (d/div {:id "main"}
+         (Header state add-item)
+         (apply d/ul {:id      "todo-list"
+                      :checked true} (->> state
+                                          :items
+                                          (map #(d/li {} (d/label {} (:text %))))))))
 
 (defn view
   "Returns a stream of view trees, represented as a core.async channel."
   [intent-chans states-chan]
-  (let [views-chan (->> (fn [state]
-                          (let [view (vtree intent-chans state)]
-                            {:view view
-                             :state state})) ; fn
-                        (map)                ; xf
-                        (chan 10)            ; ch
+  (let [views-chan (->> (fn [state] (Root state intent-chans)) ; fn
+                        (map)                                  ; xf
+                        (chan 10)                              ; ch
                         (pipe states-chan))]
     views-chan))
