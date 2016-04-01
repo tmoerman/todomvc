f3a8c8ab441d5a6315fb44a7092c69b282e9e017
Thomas Moerman <thomas.moerman@gmail.com>
2016-04-01 10:27:39 +0200

    test runner class, always re-evaluated by Figwheel

---
+++ architecture-examples/kierros/test/all/test_runner.cljs
@@ -0,0 +1,13 @@
+(ns ^:figwheel-always all.test-runner
+  (:require [kierros.async-test]
+            [kierros.core-test]
+            [kierros.util-test]
+            [todomvc-kierros.model-test]
+            [cljs.test :refer-macros [run-tests]]))
+
+(run-tests ;'kierros.core-test
+           ;'kierros.async-test
+           'kierros.util-test
+           'todomvc-kierros.model-test
+           )
+
