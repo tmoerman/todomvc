554fe10c9e243e1e975925f15f95c3f514608999
Thomas Moerman <thomas.moerman@gmail.com>
2016-04-01 10:26:14 +0200

    intent namespace

--- architecture-examples/kierros/src/todomvc_kierros/intent.cljs
+++ architecture-examples/kierros/src/todomvc_kierros/intent.cljs
@@ -1,1 +1,20 @@
-(ns todomvc-kierros.intent)
+(ns todomvc-kierros.intent
+  (:require [cljs.core.async :as a :refer [<! chan dropping-buffer]]))
+
+(def intents
+  [:navigate
+   :add-item
+   :drop-item
+   :clear-completed
+   :toggle-completed
+   :start-edit
+   :end-edit])
+
+(defn make-intent-chan [] (chan (dropping-buffer 10)))
+
+(defn intent-chans
+  "Returns a map of event channels."
+  []
+  (->> intents
+       (map (fn [key] [key (make-intent-chan)]))
+       (into {})))
