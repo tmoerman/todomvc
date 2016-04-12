5b780b6990110963063463a284a25dfbaeb8449a
Thomas Moerman <thomas.moerman@gmail.com>
2016-04-12 22:45:55 +0200

    - explicitly calling run is apparently not compatible with figwheel reloading - added history channel

--- architecture-examples/kierros/src/todomvc_kierros/core.cljs
+++ architecture-examples/kierros/src/todomvc_kierros/core.cljs
@@ -1,38 +1,34 @@
 (ns todomvc-kierros.core
   "Entry namespace for Kierros TODO-MVC app."
-  (:require [kierros.core :as cycle]
-            [cljs.core.async :as a :refer [mult tap chan sliding-buffer]]
+  (:require [cljs.core.async :as a :refer [mult tap chan pipe sliding-buffer]]
             [todomvc-kierros.intent :as i]
             [todomvc-kierros.model  :as m]
             [todomvc-kierros.view   :as v]
+            [kierros.core :as cycle]
+            [kierros.history :as h]
             [kierros.quiescent-DOM-driver :as dom]
             [kierros.local-storage-driver :as lst]))
 
 (enable-console-print!)
 
-(defn on-js-reload [] (print "on-js-reload"))
+(defn on-js-reload [] (print "js reloaded" (js/Date.)))
 
-(defn tap-buf [n mult]
-  (let [c (chan (sliding-buffer n))]
-    (tap mult c)))
+(defonce history-chan (h/init-history)) ; because of Figwheel reloading.
 
 (defn todos-cycle-main
   "Cycle main."
-  [{dom-source     :DOM
-    storage-source :STORAGE}]
-  (let [intent-chans (i/intents)
-        states-chan  (m/model storage-source intent-chans)
+  [{saved-state-chan :STORAGE}]
+  (let [intent-chans (i/intent)
+        _            (pipe history-chan (:navigate intent-chans))
+        states-chan  (m/model saved-state-chan intent-chans)
         states-mult  (mult states-chan)
-        view-states-chan   (tap-buf 10 states-mult)
-        pickle-states-chan (tap-buf 10 states-mult)
+        view-states-chan   (->> (chan) (tap states-mult))
+        pickle-states-chan (->> (sliding-buffer 1) (chan) (tap states-mult))
         views-chan   (v/view view-states-chan intent-chans)]
     {:DOM     views-chan
      :STORAGE pickle-states-chan}))
 
-(defn run
-  "Application main entry point"
-  []
-  (cycle/run
-    todos-cycle-main
-    {:DOM     (dom/make-DOM-driver "todoapp")
-     :STORAGE (lst/make-storage-driver "todoapp" m/default-state)}))
+(cycle/run
+  todos-cycle-main
+  {:DOM     (dom/make-DOM-driver "todoapp")
+   :STORAGE (lst/make-storage-driver "todomvc_kierros" m/default-state)})
